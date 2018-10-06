7454c4165a7b88356c528fa891ef5a37855a9ab8
Kislaya Ranjan <kislaya.ranjan@gmail.com>
2018-10-06 15:13:43 +0530

    Adding function to add new block

--- blockchain.py
+++ blockchain.py
@@ -5,11 +5,28 @@ class Blockchain( object ):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
-    def new_block(self):
+    def new_block(self, proof, previous_hash=None):
         """
         Creates a new block and adds it to the chain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous block
+        :return: <dict> New block 
         """
-        pass
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.last_block),
+        }
+
+        # reset current list of transactions
+        self.current_transactions = []
+
+        # add block to chain
+        self.chain.append(block)
+        return block
+
     def new_transaction(self, sender, recipient, amount):
         """
         Adds a new transaction to the list of transactions

959e3954094edde08282be70fb6172eb9aa99b35
Kislaya Ranjan <kislaya.ranjan@gmail.com>
2018-10-06 18:21:39 +0530

    Adding service endpoints using flask

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,9 @@
 import hashlib
 import json
 from time import time
+from textwrap import dedent
+from uuid import uuid4
+from flask import Flask
 
 class Blockchain( object ):
     """
@@ -102,3 +105,31 @@ class Blockchain( object ):
         guess_hash = hashlib.sha256(guess).hexdigest()
         prefix = "0" * self.difficulty
         return guess[:self.difficulty] == prefix
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+  
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
